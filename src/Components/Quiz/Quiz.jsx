6753a782e2bcf02e8a1b915e088c0fcf0b5248f2
Kanishka Anand <kanishkaadz@gmail.com>
2024-12-05 19:47:34 +0530

    Update Quiz.jsx

--- src/Components/Quiz/Quiz.jsx
+++ src/Components/Quiz/Quiz.jsx
@@ -7,16 +7,24 @@ const Quiz = () => {
     let [index, setIndex] = useState(0);
     let [question, setQuestion] = useState(data[index]);
 
+    const checkAns = (e,ans) => {
+        if (question.answer===ans) {
+            e.target.classList.add("correct");            
+        } else {
+            e.target.classList.add("wrong");
+        }
+    }
+
   return (
     <div className='container'>
         <h1>Quiz App</h1>
         <hr />
         <h2>{index+1}. {question.question}</h2>
         <ul>
-            <li>{question.option1}</li>
-            <li>{question.option2}</li>
-            <li>{question.option3}</li>
-            <li>{question.option4}</li>
+            <li onClick={(e)=>{checkAns}}>{question.option1}</li>
+            <li onClick={(e)=>{checkAns}}>{question.option2}</li>
+            <li onClick={(e)=>{checkAns}}>{question.option3}</li>
+            <li onClick={(e)=>{checkAns}}>{question.option4}</li>
         </ul>
         <button>Next</button>
         <div className="index">1 of 5 questions</div>
