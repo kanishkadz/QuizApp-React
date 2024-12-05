aad869addfc1648c5d5fa1911896205bf7d48c9f
Kanishka Anand <kanishkaadz@gmail.com>
2024-12-05 19:48:20 +0530

    Update Quiz.jsx

--- src/Components/Quiz/Quiz.jsx
+++ src/Components/Quiz/Quiz.jsx
@@ -21,10 +21,10 @@ const Quiz = () => {
         <hr />
         <h2>{index+1}. {question.question}</h2>
         <ul>
-            <li onClick={(e)=>{checkAns}}>{question.option1}</li>
-            <li onClick={(e)=>{checkAns}}>{question.option2}</li>
-            <li onClick={(e)=>{checkAns}}>{question.option3}</li>
-            <li onClick={(e)=>{checkAns}}>{question.option4}</li>
+            <li onClick={(e)=>{checkAns(e,1)}}>{question.option1}</li>
+            <li onClick={(e)=>{checkAns(e,2)}}>{question.option2}</li>
+            <li onClick={(e)=>{checkAns(e,3)}}>{question.option3}</li>
+            <li onClick={(e)=>{checkAns(e,4)}}>{question.option4}</li>
         </ul>
         <button>Next</button>
         <div className="index">1 of 5 questions</div>
