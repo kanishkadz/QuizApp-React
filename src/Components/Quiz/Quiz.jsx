8f6bb30d3e0efdee67b551f3a17e68f96518a9e8
Kanishka Anand <kanishkaadz@gmail.com>
2024-12-05 19:44:12 +0530

    Update Quiz.jsx

--- src/Components/Quiz/Quiz.jsx
+++ src/Components/Quiz/Quiz.jsx
@@ -11,12 +11,12 @@ const Quiz = () => {
     <div className='container'>
         <h1>Quiz App</h1>
         <hr />
-        <h2>1. Which device is required for Internet connection?</h2>
+        <h2>{index+1}. {question.question}</h2>
         <ul>
-            <li>Modem</li>
-            <li>Router</li>
-            <li>LAN Cable</li>
-            <li>Pen Drive</li>
+            <li>{question.option1}</li>
+            <li>{question.option2}</li>
+            <li>{question.option3}</li>
+            <li>{question.option4}</li>
         </ul>
         <button>Next</button>
         <div className="index">1 of 5 questions</div>
